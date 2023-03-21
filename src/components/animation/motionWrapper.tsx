25125a0985c436745d3eab96ccfa96e4f759c5b1
Luca <lucafrederice@gmail.com>
2023-03-21 11:37:32 -0300

    add layout id to each dashboard component

--- src/components/animation/motionWrapper.tsx
+++ src/components/animation/motionWrapper.tsx
@@ -2,12 +2,13 @@
 
 import { motion } from "framer-motion"
 
-export default function MotionWrapper({ children }: { children: React.ReactNode }) {
+export default function MotionWrapper({ children, layoutId }: { children: React.ReactNode, layoutId?: string }) {
     return (
         <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
+            layoutId={layoutId || ""}
         >
             {children}
         </motion.div>
