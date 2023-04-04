c51597cd84af11103058cc191ca9eceeddd839d3
Luca <lucafrederice@gmail.com>
2023-04-04 13:42:04 -0300

    fix

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -400,7 +400,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                                                 leaveFrom="transform opacity-100 scale-100"
                                                 leaveTo="transform opacity-0 scale-95"
                                             >
-                                                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                <Menu.Items className="z-20 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                                                     <Menu.Item>
                                                         {({ active }) => (
                                                             <a
