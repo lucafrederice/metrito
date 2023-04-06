c3f4eb4e320e7b1f61d2097ac25f3870cc191c31
Luca <lucafrederice@gmail.com>
2023-04-06 10:35:34 -0300

    push

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" className='h-full bg-gray-200'>
+    <Html lang="en" className='h-full bg-gray-200 scroll-smooth'>
       <Head />
       <body className='h-full'>
         <Main />
