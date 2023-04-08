b746414a4b4d9da4c9658f890299ebc979b2bf37
Luca Sanches Frederice <92474504+lucafrederice@users.noreply.github.com>
2023-04-08 16:58:04 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -301,7 +301,7 @@ export default function Index() {
 
                     </div>
                 </div>
-                <div ref={constraintsRef} className="fixed inset-0 pointer-events-none">
+                <div ref={constraintsRef} className="fixed inset-0 pointer-events-auto">
                     <motion.div drag dragElastic={0.1} dragMomentum={false} dragConstraints={constraintsRef} className="bg-black/75 backdrop-blur-[2px] w-fit px-6 py-4 rounded-md border border-black shadow-xl shadow-black/30 hover:shadow-xl hover:shadow-black/50 transition-shadow ease-in text-white">
                         <button onClick={() => setSharedBrands(prev => prev?.length > 0 ? [] : [1, 2, 3, 4])} >sharedBrands</button>
                     </motion.div>
